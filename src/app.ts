b3153a7afe5b81a7b80586369335bc187864afdd
alantr7 <alan.trumic066@gmail.com>
2026-03-07 15:32:11 +0100

    chore: cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { setupRoutes } from "./routes";
 import { configDotenv } from "dotenv";
+import cookieParser from "cookie-parser";
 
 configDotenv();
 
 const app = express.default();
+app.use(cookieParser());
 app.use(express.json());
 
 setupRoutes(app);
