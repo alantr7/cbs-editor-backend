64a2612e37666134e02fb3feefff3b35177cffe2
alantr7 <alan.trumic066@gmail.com>
2026-03-07 15:08:18 +0100

    chore: dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import * as express from "express";
 import { setupRoutes } from "./routes";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const app = express.default();
+app.use(express.json());
 
 setupRoutes(app);
 
